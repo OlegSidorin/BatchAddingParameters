d5563f188697628f0312e7c9f7b925346021d733
DDE3620-FQWKXS2@m1project.onmicrosoft.com <DDE3620-FQWKXS2@m1project.onmicrosoft.com>
2021-09-06 18:57:12 +0300

    wirpool

---
+++ BatchAddingParameters/Main.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.Attributes;
+using System;
+
+namespace BatchAddingParameters
+{
+    [Transaction(TransactionMode.Manual), Regeneration(RegenerationOption.Manual)]
+    public class Main : IExternalApplication
+    {
+        public static string TabName { get; set; } = "Надстройки";
+        public static string PanelTechName { get; set; } = "В семейства";
+        public Result OnStartup(UIControlledApplication application)
+        {
+            var techPanel = application.CreateRibbonPanel(PanelTechName);
+            string path = Assembly.GetExecutingAssembly().Location;
+            var MBtnData = new PushButtonData("MBtnData", "Добавь\nпараметры", path, "BatchAddingParameters.AddCommand")
+            {
+                ToolTipImage = new BitmapImage(new Uri(Path.GetDirectoryName(path) + "\\res\\bap.png", UriKind.Absolute)),
+                //ToolTipImage = PngImageSource("BatchAddingParameters.res.bap-icon.png"),
+                ToolTip = "Добавляет общие параметры в семейства, расположенные в указанной папке"
+            };
+            var TechBtn = techPanel.AddItem(MBtnData) as PushButton;
+            TechBtn.LargeImage = new BitmapImage(new Uri(Path.GetDirectoryName(path) + "\\res\\bap-icon.png", UriKind.Absolute));
+            //8TechBtn.LargeImage = PngImageSource("BatchAddingParameters.res.bap-icon.png");
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+        private System.Windows.Media.ImageSource PngImageSource(string embeddedPath)
+        {
+            Stream stream = this.GetType().Assembly.GetManifestResourceStream(embeddedPath);
+            var decoder = new System.Windows.Media.Imaging.PngBitmapDecoder(stream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+
+            return decoder.Frames[0];
+        }
+    }
+}
